0017dc1d32a3fef5a4ee58b778d8c8a00c27976e
Ben Galewsky <ben@peartreestudio.net>
2021-10-19 12:33:50 -0500

    Add unit testing of put_file_add

--- tests/servicex_did_finder_lib/test_servicex_did.py
+++ tests/servicex_did_finder_lib/test_servicex_did.py
@@ -3,3 +3,23 @@ from servicex_did_finder_lib import __version__
 
 def test_version():
     assert __version__ == '1.0.0a1'
+
+
+@responses.activate
+def test_put_file_add():
+    responses.add(responses.PUT, 'http://servicex.org/files', status=206)
+    sx = ServiceXAdapter("http://servicex.org")
+    sx.put_file_add({
+        'file_path': 'root://foo.bar.ROOT',
+        'adler32': '32',
+        'file_size': 1024,
+        'file_events': 3141
+    })
+
+    assert len(responses.calls) == 1
+    submitted = json.loads(responses.calls[0].request.body)
+    assert submitted['file_path'] == 'root://foo.bar.ROOT'
+    assert submitted['adler32'] == '32'
+    assert submitted['file_events'] == 3141
+    assert submitted['file_size'] == 1024
+
