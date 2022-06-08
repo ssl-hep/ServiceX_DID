25b79496821c7fc4b7a612d45317e591bcd878b4
Ilija Vukotic <ivukotic@cern.ch>
2022-06-07 23:41:51 -0500

    additional tests

--- tests/servicex_did_finder_lib/test_servicex_did.py
+++ tests/servicex_did_finder_lib/test_servicex_did.py
@@ -29,6 +29,48 @@ def test_put_file_add():
 
 
 @responses.activate
+def test_put_file_add_bulk():
+    responses.add(responses.PUT, 'http://servicex.org/files', status=206)
+    sx = ServiceXAdapter("http://servicex.org")
+    sx.put_file_add_bulk([{
+        'paths': ['root://foo.bar.ROOT'],
+        'adler32': '32',
+        'file_size': 1024,
+        'file_events': 3141
+    }, {
+        'paths': ['root://foo.bar1.ROOT'],
+        'adler32': '33',
+        'file_size': 1025,
+        'file_events': 3142
+    }])
+
+    assert len(responses.calls) == 1
+    submitted = json.loads(responses.calls[0].request.body)
+    assert submitted[0]['paths'][0] == 'root://foo.bar.ROOT'
+    assert submitted[0]['adler32'] == '32'
+    assert submitted[0]['file_events'] == 3141
+    assert submitted[0]['file_size'] == 1024
+
+    assert submitted[0]['paths'][0] == 'root://foo.bar1.ROOT'
+    assert submitted[0]['adler32'] == '33'
+    assert submitted[0]['file_events'] == 3142
+    assert submitted[0]['file_size'] == 1025
+
+
+@responses.activate
+def test_put_file_add_bulk_large():
+    responses.add(responses.PUT, 'http://servicex.org/files', status=206)
+    sx = ServiceXAdapter("http://servicex.org")
+    sx.put_file_add_bulk([{
+        'paths': ['root://foo.bar.ROOT'],
+        'adler32': '32',
+        'file_size': 1024,
+        'file_events': 3141
+    }] * 32)
+    assert len(responses.calls) == 2
+
+
+@responses.activate
 def test_put_file_add_with_prefix():
     responses.add(responses.PUT, 'http://servicex.org/files', status=206)
     sx = ServiceXAdapter("http://servicex.org", "xcache123:")
